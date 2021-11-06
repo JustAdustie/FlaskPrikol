b76b0a0fa4af090738016832608564f4d147ef75
Gradgeron <o.artemka@gmail.com>
2021-11-06 17:08:32 +0300

    dbupdate

--- FlaskDBandSite/main.py
+++ FlaskDBandSite/main.py
@@ -1,24 +1,60 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for,request, redirect
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLACHEMY_DATABASE_URI'] = ''
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///main.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
-@app.route('/')
-@app.route('/home')
+
+class Auth(db.Model):
+    login = db.Column(db.String(), primary_key=True, nullable=False)
+    password = db.Column(db.String(), primary_key=True, nullable=False)
+
+    def __repr__(self):
+        return '<Auth %r>' % self.login
+
+    def __repr__(self):
+        return '<Auth %r>' % self.password
+
+
+class CardAuth(db.Model):
+    id = db.Column(db.String(), primary_key=True, nullable=False)
+
+    def __repr__(self):
+        return '<CardAuth %r>' % self.id
+
+
+@app.route('/', methods=['POST', 'GET'])
 def index():
-    return render_template("index.html")
+    if request.method == "POST":
+        login = request.form['login']
+        password = request.form['password']
+        auth = Auth(login=login, password=password)
+
+        try:
+            db.session.add(auth)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return "Logging error."
+    else:
+        return render_template("index.html")
 
 
-@app.route('/about')
-def about():
-    return render_template("about.html")
+@app.route('/guard')
+def guard():
+    return render_template("guard.html")
 
 
-@app.route('/user/<string:name>/<int:id>')
-def user(name, id):
-    return render_template("admin.html")
+@app.route('/admin')
+def admin():
+    all_auth = Auth.query.order_by(Auth.login).all()
+    return render_template("admin.html", allauth=all_auth)
 
+
+#@app.route('/admin/<string:login><string:password>/delete')
+#def authdelete(login, password):
 
 if __name__ == "__main__":
     app.run(debug=True)
