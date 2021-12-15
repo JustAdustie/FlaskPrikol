0e589ad2a5a1f8a2cda2402310d3e65fb00c52ce
JustAdustie <58479779+JustAdustie@users.noreply.github.com>
2021-12-15 20:11:47 +0300

    fastfix:/

--- FlaskDBandSite/__init__.py
+++ FlaskDBandSite/__init__.py
@@ -6,7 +6,7 @@ from flask_login import LoginManager
 db = SQLAlchemy()
 def create_app():
     app = Flask(__name__) 
-    app.config['SECRET_KEY'] = 'ebaniysite' 
+    app.config['SECRET_KEY'] = 'yadahsdhqiowdquqh981h298eh19daljksnd' 
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False 
     db.init_app(app) 
